74c9a46431f0263676cd98e699fd94f4f32a944c
reham611 <117862392+reham611@users.noreply.github.com>
2023-01-25 10:36:45 -0800

    fristt

---
+++ chatgpt/chatgpt/lib/main.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chatbot',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ChatbotScreen(),
+    );
+  }
+}
+
+class ChatbotScreen extends StatefulWidget {
+  @override
+  _ChatbotScreenState createState() => _ChatbotScreenState();
+}
+
+class _ChatbotScreenState extends State<ChatbotScreen> {
+  final TextEditingController _textController = TextEditingController();
+  final List<ChatMessage> _messages = [];
+  final Uri _openAIUrl = Uri.parse('https://api.openai.com/v1/completions');
+  final String _openAIKey =
+      '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';
+
+  void _sendMessage() {
+    if (_textController.text.trim().isEmpty) {
+      return;
+    }
+
+    ChatMessage message = ChatMessage(
+      text: _textController.text,
+      sender: 'user',
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+    _textController.clear();
+    _getResponse(message.text);
+  }
+
+  void _getResponse(String message) async {
+    Map<String, String> headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $_openAIKey',
+    };
+    Map<String, dynamic> body = {
+      'model': 'text-davinci-002',
+      'prompt': message,
+      'max_tokens': 256,
+    };
+    http.Response response =
+        await http.post(_openAIUrl, headers: headers, body: json.encode(body));
+    Map<String, dynamic> responseJson = json.decode(response.body);
+    print(responseJson);
+    ChatMessage chatMessage = ChatMessage(
+      text: responseJson['choices'][0]['text'],
+      sender: 'bot',
+    );
+    setState(() {
+      _messages.insert(0, chatMessage);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chatbot'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            child: ListView.builder(
+              reverse: true,
+              padding: EdgeInsets.all(8.0),
+              itemBuilder: (_, int index) => _messages[index],
+              itemCount: _messages.length,
+            ),
+          ),
+          Divider(height: 1.0),
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).cardColor,
+            ),
+            child: _buildTextComposer(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                decoration:
+                    InputDecoration.collapsed(hintText: 'Send a message'),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                  icon: Icon(Icons.send), onPressed: () => _sendMessage()),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  final String text;
+  final String sender;
+
+  ChatMessage({required this.text, required this.sender});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(
+              child: Text(sender[0]),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                sender,
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
